f541656d2740a5befc158598acba1b8a0d4353cc
Orest <oresto101@gmail.com>
2023-08-23 19:51:11 +0200

    Initial commit

---
+++ src/main/java/com/vako/JellyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vako;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JellyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JellyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vako/JellyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vako;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JellyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
