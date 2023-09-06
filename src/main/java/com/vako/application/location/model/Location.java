80f94af4b2338fca51e7c077a11bd5d75a63a1be
Orest <oresto101@gmail.com>
2023-09-06 17:45:46 +0200

    Initial

--- src/main/java/com/vako/application/location/model/Location.java
+++ src/main/java/com/vako/application/location/model/Location.java
@@ -1,9 +1,6 @@
 package com.vako.application.location.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -13,7 +10,7 @@ import java.math.BigDecimal;
 @Table(name = "location")
 public class Location {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userId;
     private BigDecimal latitude;
