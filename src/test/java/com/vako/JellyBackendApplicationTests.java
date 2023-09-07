dada07bdc497f488545da858019165ad656f4e5d
Orest <oresto101@gmail.com>
2023-09-07 23:54:15 +0200

    Initial

--- src/test/java/com/vako/JellyBackendApplicationTests.java
+++ src/test/java/com/vako/JellyBackendApplicationTests.java
@@ -1,9 +1,11 @@
 package com.vako;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class JellyBackendApplicationTests {
 
     @Test
