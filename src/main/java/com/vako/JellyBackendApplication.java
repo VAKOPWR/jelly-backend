849d517624d0d335a7a7350f4037651824fddc01
Orest <oresto101@gmail.com>
2023-10-09 02:49:10 +0200

    ZV-14: implement security

--- src/main/java/com/vako/JellyBackendApplication.java
+++ src/main/java/com/vako/JellyBackendApplication.java
@@ -1,12 +1,27 @@
 package com.vako;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @SpringBootApplication
 public class JellyBackendApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        FileInputStream serviceAccount =
+                new FileInputStream("src/main/resources/service-account-key.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
         SpringApplication.run(JellyBackendApplication.class, args);
     }
 
