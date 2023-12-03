f310aff071ee22192fae29c3d6228dc9b7e7932d
Orest <oresto101@gmail.com>
2023-12-03 23:45:43 +0100

    NONE: fixes

--- src/main/java/com/vako/application/config/FirebaseConfig.java
+++ src/main/java/com/vako/application/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.vako.application.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,13 +14,14 @@ import java.io.IOException;
 public class FirebaseConfig {
 
     @Bean
-    public void initializeFirebase() throws IOException {
+    public FirebaseMessaging initializeFirebase() throws IOException {
         FileInputStream serviceAccount = new FileInputStream("src/main/resources/service-account-key.json");
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                 .build();
-
         FirebaseApp.initializeApp(options);
+        return FirebaseMessaging.getInstance();
     }
+
 }
